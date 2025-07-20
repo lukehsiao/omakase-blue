9b616d1fbdb1df7248ba1352822efb0c4a7b0e4d
Luke Hsiao <luke@hsiao.dev>
2025-07-20 09:21:51 -0600

    chore(config/wezterm): remove aggressive github hyperlink rule

--- configs/wezterm.lua
+++ configs/wezterm.lua
@@ -40,13 +40,4 @@ config.adjust_window_size_when_changing_font_size = false
 
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
 
--- make username/project paths clickable. this implies paths like the following are for github.
--- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
--- as long as a full url hyperlink regex exists above this it should not match a full url to
--- github or gitlab / bitbucket (i.e. https://gitlab.com/user/project.git is still a whole clickable url)
-table.insert(config.hyperlink_rules, {
-  regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
-  format = 'https://www.github.com/$1/$3',
-})
-
 return config
